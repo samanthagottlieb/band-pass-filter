a0cc07fb3949f30b2cb49f30baf06288caf229f1
Samantha <samanthagottlieb31@gmail.com>
2020-12-22 00:28:54 +0000

    Change unit test to use context
    To keep tests clear and consistent

--- spec/music_filter_spec.rb
+++ spec/music_filter_spec.rb
@@ -1,8 +1,10 @@
 require 'music_filter'
 
 describe 'music_filter' do
-  it 'returns an unaltered array when integers are within thresholds' do
-    expect(music_filter([20, 40, 60], 20, 60)).to eq([20, 40, 60])
+  context 'when array integers are within low and high thresholds' do
+    it 'returns an unaltered array ' do
+      expect(music_filter([20, 40, 60], 20, 60)).to eq([20, 40, 60])
+    end
   end
 
   context 'when passed an array with an integer below the low pass filter' do
